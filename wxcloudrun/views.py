fa773a222b15248cf2ce8c83da1e32b89a422ed3
wzkk123 <85175372+wzkk123@users.noreply.github.com>
2022-03-31 19:46:02 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -161,14 +161,14 @@ def upload():
     print(changtoushis)
     return changtoushis
 
-@app.route('/upload1', methods=['POST'])
-def upload1():
-    global xuxieshi1
-    text = request.form.get('text')
-    print(text)
-    acrostic = json.loads(text)
-    print(acrostic)
-    xuxieshi1 = show1(result=acrostic, prefix='明日何其多，总管一二三。', net = net)
-    print(xuxieshi1)
-    return xuxieshi1
+# @app.route('/upload1', methods=['POST'])
+# def upload1():
+#     global xuxieshi1
+#     text = request.form.get('text')
+#     print(text)
+#     acrostic = json.loads(text)
+#     print(acrostic)
+#     xuxieshi1 = show1(result=acrostic, prefix='明日何其多，总管一二三。', net = net)
+#     print(xuxieshi1)
+#     return xuxieshi1
 
